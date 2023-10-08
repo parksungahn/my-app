48f1167e5bd8e50857b60e6751184cad5a177a99
parksungahn <appzzang@gmail.com>
2023-10-09 02:17:20 +0900

    test

--- src/pages/Exam_new/New_Test.js
+++ src/pages/Exam_new/New_Test.js
@@ -1,36 +1,51 @@
 import React, {useCallback, useEffect, useMemo, useRef, useState} from "react";
 import {queries} from "@testing-library/react";
 
+
 const New_Test = () =>{
 
-    const [number, setNumber] = useState(0);
 
-    let person = [
-        {carr: "KE", fltnum: "01"},
-        {carr: "", fltnum: "0002"},
-    ];
 
-    const flightScheduleList = useMemo(() =>
-                person?.map(
-                    ({
-                         carr:{ carr = 'KE' },
-                         fltnum ,
-                     }) => ({
-                        carr: carr,
-                        fltnum: `${fltnum.padStart(4, '0')}`,
-                        date:'09SEP73'
-                    }),
-            ) ?? [],
-        [number],
-    );
 
-    console.log(flightScheduleList);
+
+
+
+    //----Object.assign 으로 객체복사  ... 스프레드 사용
+    let user = {name: 'Mike'};
+    let info = {age: 30};
+    let fe = ['Js', 'React'];
+    let lang = ['Korean', 'English'];
+
+    // user = Object.assign({}, user, info, {
+    //     skills: [],
+    // });
+    //
+    // fe.forEach((item)=> {
+    //     user.skills.push(item);
+    // })
+    // lang.forEach((item)=>{
+    //     user.skills.poush(item);
+    // })
+
+    //--아래방법으로 한번에 해결, 스프레드 ... 사용
+     user = {
+         ...user,
+         ...info,
+         skills: [...fe, ...lang],
+     };
+
+    console.log(user);
+
+
+
+
+
+
 
     return(
-        <div>
-            <input type="number" onChange={(e)=>setNumber(parseInt(e.target.value))}/>
-        </div>
+        <div></div>
     );
 };
+
 
 export default New_Test;
